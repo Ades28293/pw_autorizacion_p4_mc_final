451d453a8e93f3c7461e40240ccd91224f2cc904
Michael Cen <cenyungamichael@gmail.com>
2023-08-22 15:29:09 -0500

    Arreglo Inyeccion

---
+++ src/test/java/com/example/demo/PwAutorizacionP4McFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwAutorizacionP4McFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
